2a766c80208e600fa454055f65f75f773b7afd62
Gabriel <juanmendez0013@gmail.com>
2024-12-26 10:57:39 -0300

    first commit

---
+++ src/items/items.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateItemInput, UpdateItemInput } from './dto/inputs';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+  ) {}
+
+  async create(createItemInput: CreateItemInput): Promise<Item> {
+    const newItem = this.itemsRepository.create(createItemInput);
+
+    return await this.itemsRepository.save(newItem);
+  }
+
+  async findAll(): Promise<Item[]> {
+    // Todo: Filtrar, paginar, por usuario
+    return this.itemsRepository.find();
+  }
+
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+
+    if (!item) throw new NotFoundException(`Item with id ${id} not found`);
+
+    return item;
+  }
+
+  update(id: number, updateItemInput: UpdateItemInput) {
+    return `This action updates a #${id} item`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} item`;
+  }
+}
